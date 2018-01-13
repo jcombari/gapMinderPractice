cbc8c60842397769c3f5e3814d8b884ef0456495
wim <wfjvdham@gmail.com>
2018-01-13 10:54:04 -0500

    add example shiny app

---
+++ app/explore/app.R
@@ -0,0 +1,40 @@
+library(shiny)
+library(tidyverse)
+
+ui <- fluidPage(
+   titlePanel("Old Faithful Geyser Data"),
+   sidebarLayout(
+      sidebarPanel(
+        uiOutput("radio")
+      ),
+      mainPanel(
+         plotOutput("acreedor")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  data <- readRDS("../../data/data_tidy.rds")
+   
+  output$acreedor <- renderPlot({
+    if (!is.null(input$select_mes)) {
+      data_graph <- data %>%
+        filter(MES %in% input$select_mes)
+      ggplot(data_graph) +
+        geom_bar(aes(acreedor))
+    }
+  })
+  
+  output$radio <- renderUI({
+    choices <- setNames(unique(data$MES), unique(data$MES))
+    checkboxGroupInput("select_mes", label = h3("Mes"), 
+                       choices = choices,
+                       selected = "ENERO")
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
